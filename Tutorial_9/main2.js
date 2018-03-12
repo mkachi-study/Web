9f2bfd3034df9eab4f7279450ee28b3233880c81
MKachi <mkachi@naver.com>
2018-03-12 23:41:29 +0900

    Add Tutorial_9

---
+++ Tutorial_9/main2.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+var infile = fs.createReadStream("./output.txt", { flags: "r" });
+var outFile = fs.createWriteStream("./output2.txt", { flags: "w" });
+
+infile.on("data", function(data) {
+	console.log("번째로 읽어 들인 데이터", data.toString());
+	outFile.write(data);
+});
+
+infile.on("end", function() {
+	console.log("파일 읽기 종료");
+	outFile.end(function() {
+		console.log("파일 쓰기 종료");
+	});
+});
