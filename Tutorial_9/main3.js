e822555b0c885c7eb0df7dd05277158ccc1483e6
MKachi <mkachi@naver.com>
2018-03-14 15:20:26 +0900

    Add main4, main5

--- Tutorial_9/main3.js
+++ Tutorial_9/main3.js
@@ -5,10 +5,15 @@ var outname = "./output2.txt";
 
 fs.exists(outname, function(exists) {
 	if (exists) {
+		// unlink 파일 삭제함
 		fs.unlink(outname, function(err) {
 			if (err) {
 				throw err;
 			}
 		});
 	}
+	var infile = fs.createReadStream(inname, { flags: "r" });
+	var outfile = fs.createWriteStream(outname, { flags: "w" });
+	infile.pipe(outfile);
+	console.log("파일 복사 [" + inname + "] -> [" + outname + "]");
 });
