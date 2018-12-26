79df76e0963ae7aaaf077638e6f9dae84dfd2ab0
MKachi <mkachi@naver.com>
2018-12-26 17:48:04 +0900

    Add tutoria4 - audio

---
+++ assets/js/tutorial_4.js
@@ -0,0 +1,16 @@
+var bgm = new Audio("../audio/bgm.mp3");
+bgm.loop = true;
+bgm.play();
+
+var effect = new Audio("../audio/se.mp3");
+effect.currentTime = 0;
+
+
+document.addEventListener("keydown", keyDown, false);
+
+function keyDown(event) {
+    if (event.keyCode == 32) {
+        effect.currentTime = 0;
+        effect.play();
+    }
+}
