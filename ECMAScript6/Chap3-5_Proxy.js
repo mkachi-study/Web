f567ca05b1a68fae967f12c096ac5f85bbc6de98
MKachi <mkachi@naver.com>
2019-03-05 15:38:08 +0900

    Update Chap3-5

--- ECMAScript6/Chap3-5_Proxy.js
+++ ECMAScript6/Chap3-5_Proxy.js
@@ -1,12 +1,20 @@
-let target = {};
+function target() { }
+target.prototype.a = 10;
+target.prototype.b = "Test";
+
 let handler = {
-	set: function(target, key, value, receiver) {
+	set: function (target, key, value, receiver) {
 		console.log(key, value);
+		console.log("set trap");
+
+		// Relfect는 trap과 같은 api를 정적 메서드로 지니고 있다.
+		// trap이 동작을 가로첸 후 Relfect가 원래의 동작을 수행하게 해준다.
 		return Reflect.set(target, key, value, receiver);
 	}
 };
 
 let proxy = new Proxy(target, handler);
-proxy.name = "Test Proxy";
 
 console.log(target);
+console.log("=== data set ===");
+target.a = 10;
