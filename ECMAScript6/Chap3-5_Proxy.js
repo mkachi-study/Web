8d0288ae0401100f0c8ca1e5ed3eb90e06a0b093
맛카치 <mkachi@naver.com>
2019-03-17 15:59:22 +0900

    Mearge Project'

--- ECMAScript6/Chap3-5_Proxy.js
+++ ECMAScript6/Chap3-5_Proxy.js
@@ -1,12 +1,1 @@
-let target = {};
-let handler = {
-	set: function(target, key, value, receiver) {
-		console.log(key, value);
-		return Reflect.set(target, key, value, receiver);
-	}
-};
-
-let proxy = new Proxy(target, handler);
-proxy.name = "Test Proxy";
-
-console.log(target);
+//
